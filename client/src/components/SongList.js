e0182faf695da32b06070015462e56caa032a5bb
Brandon Diep <brandondiep2711@gmail.com>
2025-01-15 18:47:36 -0800

    add playlist state for playlist creation

---
+++ client/src/components/SongList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Song = ({ song }) => {
+  return (
+    <div>Song</div>
+  )
+}
+
+export default Song
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,11 +2,12 @@ import { useState, useEffect } from 'react';
 import { getGenres } from '../services/openaiService'
 import { getSpotifySongs } from '../services/spotifyService'
 import { isUserAuthenticated } from '../utils/authUtils';
-import { Play } from 'lucide-react';
+import { Play, CircleX, CirclePlus   } from 'lucide-react';
 import Player from '../components/Player';
 const Home = () => {
     const [mood, setMood] = useState("")
     const [genres, setGenres] = useState([])
+    const [playlist, setPlaylist] = useState([])
     const [songs, setSongs] = useState([])
     const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [playingTrack, setPlayingTrack] = useState()
@@ -40,6 +41,14 @@ const Home = () => {
 
     const handlePlayingTrack = (trackUri) => {
       setPlayingTrack(trackUri)
+    }
+
+    const handleTogglePlaylistTrack = (track) => {
+      setPlaylist((prevPlaylist) => 
+        prevPlaylist.some((song) => song.id === track.id) ?
+        prevPlaylist.filter((song) => song.id !== track.id) :
+        [...prevPlaylist, track]
+      )
     }
 
     const formatDuration = (millis) => {
@@ -78,15 +87,15 @@ const Home = () => {
                 </ul>
               </div>
              )}
-       
-            {songs.length > 0 && (
-              <>
-                <div>
+
+            <div className='grid grid-cols-3 gap-24'>
+              {songs.length > 0 && (
+                <div className='col-span-2'>
                   <h2>Recommended Songs:</h2>
                   <ul className='grid grid-cols-1 gap-5'>
-                    {songs.map((song) => (
-                        <li key={`${song.id}-${song.uri}`} className='flex items-center'>
-                          <div className='relative group' onClick={() => handlePlayingTrack(song.uri)} >
+                    {[...new Map(songs.map((song) => [song.uri, song])).values()].map((song) => (
+                        <li key={`${song.id}`} className='flex items-center relative group' onClick={() => handlePlayingTrack(song.uri)}>
+                          <div className='relative group' >
                             <img 
                               src={song.album.image} 
                               alt={`album cover ${song.name}`} 
@@ -102,22 +111,76 @@ const Home = () => {
                             <p>{song.artists.join(", ")}</p>
                           </div>
 
-                          <div className='flex flex-col'>
+                          <div className='flex flex-row gap-6 not:group-hover'>
+                            <button onClick={(e) => {
+                                e.stopPropagation()  // prevent li onClick from being triggered
+                                handleTogglePlaylistTrack(song)
+                              }}>
+                              {playlist.some((track) => track.id === song.id) ? 
+                              (                        
+                                <CircleX />
+                              )
+                              : 
+                               <CirclePlus className='hover:text-white' />
+                              }
+                            </button>
                             {formatDuration(song.duration)}
-                            <button onClick={() => handlePlayingTrack(song.uri)}>+</button>
+                            
                           </div>
                           
                         </li>
                       ))}
                     </ul>
-                </div>
+                </div>  
+              )}
 
-                <div>
+              <aside>
+                <h2>Playlist</h2>
+                <ul className='grid grid-cols-1  gap-5'>
+                  {playlist && playlist.map((song) => (
+                      <li key={`${song.id}`} className='flex items-center relative group' onClick={() => handlePlayingTrack(song.uri)}>
+                        <div className='relative group' >
+                          <img 
+                            src={song.album.image} 
+                            alt={`album cover ${song.name}`} 
+                            className='rounded hover:opacity-75' 
+                          />
+                          <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                            <i className='text-white'> <Play /></i>
+                          </div>
+                        </div>
 
-                </div>
-                
-              </>
-            )}
+                        <div className='flex-grow pl-4'>
+                          <h2 className='font-bold'>{song.name}</h2>
+                          <p>{song.artists.join(", ")}</p>
+                        </div>
+
+                        <div className='flex flex-row gap-6 not:group-hover'>
+                          <button onClick={(e) => {
+                              e.stopPropagation()  // prevent li onClick from being triggered
+                              handleTogglePlaylistTrack(song)
+                            }}>
+                            {playlist.some((track) => track.id === song.id) ? 
+                            (                        
+                              <CircleX className='hover:text-white' />
+                            )
+                            : 
+                             <CirclePlus className='hover:text-white' />
+                            }
+                          </button>
+                          {formatDuration(song.duration)}
+                          
+                        </div>
+                        
+                      </li>
+                  ))}
+                </ul> 
+                <button className="btn btn-block mt-5">Create Playlist</button>
+  
+              </aside>
+
+            </div>
+            
             <div className="player-wrapper">
               <Player trackUri={playingTrack} />
             </div>
