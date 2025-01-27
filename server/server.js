4e31a54691f480c3185d2e8f7b4a4a64d23519e2
Brandon Diep <brandondiep2711@gmail.com>
2025-01-26 22:51:25 -0800

    added sentimusic.onrender to CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Enable CORS for client origin only
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://sentimusic.onrender.com'],
 }
 app.use(cors(corsOptions))
 
